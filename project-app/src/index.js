dd9a8ac4ae8e4c00188b3034a23d0d36014c80be
AndreaPortilla24 <144179901+AndreaPortilla24@users.noreply.github.com>
2023-11-28 00:12:29 -0500

    Delete project-app directory
    Eliminacion para agregacion de nuevo proyecto

--- project-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
-import App from "./App";
-
-
-
-//Se renderiza el objeto root haciento el llamado a app.js
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
